8092c4a85348874e16f5b0d0800466d493247f87
paulo.barroz <paulo.barroz@pcmcomercio.lan>
2023-05-25 11:49:15 -0300

    projectcreate

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,6 +10,8 @@ export class SmallCardComponent implements OnInit {
   PhotoCover:string = ""
   @Input()
   cardTitle:string = ""
+  @Input()
+  Id:string = "0"
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,22 @@
+export const dataFake = [
+  {"id":'0',
+  "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlHiZeg06bWWDQzeJe6MdqItU_ZJ5KLCTTKg&usqp=CAU",
+  "title": "SAIU NOVO FILME VAMOS APROVEITAR É MUITO TOP",
+  "description": "olaaa"},
+
+  {"id":'1',
+"photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlHiZeg06bWWDQzeJe6MdqItU_ZJ5KLCTTKg&usqp=CAU",
+"title": "SAIU NOVO FILME VAMOS APROVEITAR É MUITO TOP",
+"description": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"},
+
+{"id":'2',
+"photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW1W1RuDjOzH9kf88v-yI3nfM3O1Md_gy4Cw&usqp=CAU",
+"title": "saiu novo filme",
+"description": "HIIIIIIIIIIIIIIIIIIIIIII"}
+
+
+
+
+]
+
+
